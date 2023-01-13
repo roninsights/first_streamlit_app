8c33e9c9e33965deb9eea479bc66ff3d92aef8c8
roninsights <62627868+roninsights@users.noreply.github.com>
2023-01-13 12:39:33 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,9 +54,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 
-streamlit.stop()
-
-
 streamlit.header("The fruit load list contains")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
